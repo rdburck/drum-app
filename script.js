f06c3e6ab8b161d2475746c8258b345cf89b02c6
rdburck <rdburck@gmail.com>
2021-05-26 08:09:45 -0700

    added more javaScript to get a working app

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown', function(keyboardEvent) {
-    const audio = document.querySelector(`audio[data-key="${keyboardEvent.keyCode}"]`)
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(!audio) return;
-    console.log(audio, keyboardEvent.keyCode);
+    console.log(audio, e.keyCode);
     audio.currentTime = 0
     audio.play();
-    key.add.classList('playing');
-})
+    key.classList.add('playing');
+}
+
+function removeTransition(e){
+    if(e.propertyName !== transform) return;
+    this.classList.remove('playing');
+}
+
+const keys = Array.from(document.querySelector('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
